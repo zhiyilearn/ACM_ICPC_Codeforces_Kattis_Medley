86daaa198859c5352141bc5150424776e9a81bc4
Zhiyi Li <zli04@vt.edu>
2019-03-14 22:06:19 -0400

    Add files via upload

---
+++ ProblemF.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ProblemF {
+    public static void main(String[] argc)
+    {
+        Scanner sc = new Scanner(System.in);
+        int xs, ys, ss, ri, rf;
+        xs = sc.nextInt();
+        ys = sc.nextInt();
+        ss = sc.nextInt();
+        ri = sc.nextInt();
+        rf = sc.nextInt();
+
+        int xa, ya, sa;
+        xa = sc.nextInt();
+        ya = sc.nextInt();
+        sa = sc.nextInt();
+
+        double D = Math.sqrt((xa - xs)*(xa - xs) + (ya - ys)*(ya - ys));
+        double T;
+        sa * T + ri - ss * T =
+
+    }
+}
